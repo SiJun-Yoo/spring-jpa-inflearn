66f3deb61926ebf248cf9ebc072fada4ace286fe
SiJun-Yoo <simba0502@naver.com>
2022-05-16 21:20:24 +0900

    김영한 Spring-JPA 강의 3강까지

--- src/main/java/jpabook/jpashop/controller/HelloController.java
+++ src/main/java/jpabook/jpashop/controller/HelloController.java
@@ -1,2 +1,14 @@
-package jpabook.jpashop.controller;public class HelloController {
+package jpabook.jpashop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping
+    public String hello(Model model){
+        model.addAttribute("data","hihihi");
+        return "hello";
+    }
 }
