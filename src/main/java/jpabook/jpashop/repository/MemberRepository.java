ac98f5f9afb502781ab520d3c4792000ad7fa21b
DESKTOP-G3USBLG\simba <simba0502@naver.com>
2022-05-18 13:06:40 +0900

    김영한 springboot-jpa강의 중간 push

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,2 +1,26 @@
-package jpabook.jpashop.repository;public class MemberRepository {
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        /*
+        굳이 member를 리턴하지 않는이유는 memeber를 리턴하여 발생할수 있는 사이드 이펙트를 예방하기 위해서 이다.
+        return을 아무것도 하지 않거나 id정도만 return하여 추후 조회시 활용함
+         */
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class,id);
+    }
 }
--- src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -1,4 +1,37 @@
-import static org.junit.jupiter.api.Assertions.*;
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class MemberRepositoryTest {
-  
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setName("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+    }
 }
